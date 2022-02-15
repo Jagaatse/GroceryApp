7938593ccf3813df9a3faa8c3fe7b3cd26c260cd
Jagaatse <jagaatse03@gmail.com>
2022-02-15 17:22:37 +0800

    Grocery App

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+ <!--- Footer section starts--->
+ <div class="Footer">
+            <div class="wrapper">
+                <p class="text-center">JAGAA-MART India © All rights Reserved</p>
+            </div>
+        </div>
+        <!--- Footer section edns--->
+
+
+                
+    </body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,25 @@
+
+
+<html>
+    <head>
+        <title>Grocery Order Website - Home Page</title>
+        
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    
+    <body>
+        <!--- Menu section starts--->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-grocery.php">Grocery</a><li>
+                    <li><a href="manage-order.php">Order</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--- Menu section ends--->
+        
+        <!--- Main content section starts--->
